83f4ee5ace3c9815bce7e7ebf2ac3d1156b25c19
Munkittaja <joni.kurki@gmail.com>
2019-05-09 22:05:09 +0300

    Added mouse following selector, which 'pulsates'

---
+++ Assets/Scirpts/ChunkBuilderMouseAnimation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkBuilderMouseAnimation : MonoBehaviour
+{
+    public float _animationTickThreshold;
+
+    public bool _isEnabled = true;
+
+    const float _animationTick = .05f;
+    const float _maxSize = 1f;
+    const float _minSize = .65f;
+    
+    private float _lastCheck;
+    private bool _isScalingDown = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lastCheck = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isEnabled && (Time.time > _lastCheck + _animationTickThreshold))
+        {
+            if (_isScalingDown)
+            {
+                transform.localScale = new Vector3(transform.localScale.x - _animationTick, transform.localScale.y - _animationTick, 0);
+
+                if (transform.localScale.x <= _minSize)
+                    _isScalingDown = false;
+
+            }
+            else
+            {
+                transform.localScale = new Vector3(transform.localScale.x + _animationTick, transform.localScale.y + _animationTick, 0);
+
+                if (transform.localScale.x >= _maxSize)
+                    _isScalingDown = true;
+            }
+            _lastCheck = Time.time;
+        }
+    }
+}
--- Assets/Scirpts/ChunkBuilderScript.cs
+++ Assets/Scirpts/ChunkBuilderScript.cs
@@ -8,6 +8,8 @@ public class ChunkBuilderScript : MonoBehaviour
     [SerializeField]
     public bool _isEnabled = true;
 
+    public bool _validBuild = true;
+
     const float MOUSE_OFFSET = 0;
 
     // Start is called before the first frame update
@@ -19,20 +21,35 @@ public class ChunkBuilderScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_isEnabled)
+        if (_isEnabled && _validBuild)
         {
             GetComponent<SpriteRenderer>().color = new Color(0, 255, 0, .8f);
 
             var point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            
             transform.position = new Vector3(Mathf.RoundToInt(point.x + MOUSE_OFFSET), Mathf.RoundToInt(point.y + MOUSE_OFFSET), 0);
-
-            
         }
-        else
+        else if(_isEnabled && !_validBuild)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(255, 0, 0, .8f);
+
+            var point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            transform.position = new Vector3(Mathf.RoundToInt(point.x + MOUSE_OFFSET), Mathf.RoundToInt(point.y + MOUSE_OFFSET), 0);
+        }
+        else if(!_isEnabled)
         {
             GetComponent<SpriteRenderer>().color = Color.clear;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+
+        }
+
+        // TODO for test
+        if (Input.GetKeyDown(KeyCode.Mouse1)){
+            _validBuild = !_validBuild;
         }
     }
 }
